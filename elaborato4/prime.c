eba40835f9794b03e1e88a67935b7a04af936ea8
vitaliklevchenko <vitalik72004@gmail.com>
2024-10-16 15:03:02 +0200

    first

---
+++ elaborato4/prime.c
@@ -0,0 +1,22 @@
+#include "prime.h"
+
+unsigned short int is_prime(unsigned short int n) {
+
+	return  0;
+}
+
+unsigned short int nth_prime(unsigned short int n) {
+	return  0;
+}
+
+
+
+unsigned short int succ_prime(int reset) {
+	return  0;
+}
+
+
+unsigned short int co_prime(unsigned short int m, unsigned short int n) {
+	return 0;
+}
+
