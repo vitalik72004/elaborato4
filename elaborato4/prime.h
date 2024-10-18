98384baeaafe9ab6822308b0f1b0c3c4c706aa74
vitaliklevchenko <vitalik72004@gmail.com>
2024-10-18 21:41:17 +0200

    pre final mancano i commenti

--- elaborato4/prime.h
+++ elaborato4/prime.h
@@ -1,6 +1,8 @@
 #include <math.h>
 #include <limits.h>
 
+unsigned short int current_int = 2;
+
 unsigned short int is_prime(unsigned short int n) {
 	int i;
 	if (n <= 1) return 0;
@@ -32,11 +34,41 @@ unsigned short int nth_prime(unsigned short int n) {
 
 
 unsigned short int succ_prime(int reset) {
-	return  0;
+	if (reset != 0)
+	{
+		current_int = 2;
+		return current_int;
+	}
+
+	while (1)
+	{
+		if (is_prime(current_int))
+		{
+			unsigned short int res = current_int;
+			current_int++;
+
+			if (res > USHRT_MAX)
+			{
+				current_int = 2;
+				return 0;
+			}
+			return res;
+		}
+		current_int++;
+	}
 }
 
 
 unsigned short int co_prime(unsigned short int m, unsigned short int n) {
-	return 0;
+	unsigned short int temp;
+	while(n!=0)
+	{
+		temp = n;
+		n = m % n;
+		m = temp;
+	}
+
+	if (m == 1) return 1;
+	else return 0;
 }
 
